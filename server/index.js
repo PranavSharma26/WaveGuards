2d1bffedb4006cca53fab70933497661f38929d9
Pranav Sharma <pranavsharma12355@gmail.com>
2025-11-05 01:07:47 +0530

    bug fix for CORS

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 const port = process.env.PORT || 3000
 const app = express()
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 app.use(express.json())

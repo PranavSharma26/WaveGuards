685e055d8b5bc67722d3fd6719e219c33137fbcf
Pranav Sharma <pranavsharma12355@gmail.com>
2025-11-13 18:05:42 +0530

    app.options issue  fixed

--- server/index.js
+++ server/index.js
@@ -21,7 +21,18 @@ app.use(cors({
     ],
     credentials: true
 }))
-app.options("*", cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
 app.use(express.json())
 app.use(cookieParser())
 

26ec030331b3050f4fb3961f781f5ef9ceca62ee
Pranav Sharma <pranavsharma12355@gmail.com>
2025-11-13 18:36:28 +0530

    Fixed CORS issue by including 'Patch' method in CORS

--- server/index.js
+++ server/index.js
@@ -13,24 +13,19 @@ dotenv.config()
 
 const port = process.env.PORT || 3000
 const app = express()
-// app.use(cors({
-//     origin: [
-//         "http://localhost:5173",
-//         "http://51.21.245.146",
-//         "https://waveguards.vercel.app"
-//     ],
-//     credentials: true
-// }))
 app.use(cors({
   origin: [
-      "http://localhost:5173",
-      "http://51.21.245.146",
-      "https://waveguards.vercel.app"
+    "http://localhost:5173",
+    "http://51.21.245.146",
+    "https://waveguards.vercel.app"
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors());
+
 
 app.use(express.json())
 app.use(cookieParser())
